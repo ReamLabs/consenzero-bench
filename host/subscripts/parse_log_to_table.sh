0a33dc696c7141e9dfed3ca5c1ff18a85d7d26c5
Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>
2025-04-11 15:43:45 +0700

    Benchmark beacon STFs (#4)
    * feat: reimplement in risc0
    
    * fix: bump cargo to 2024 and rustc to 1.85.0 to stay compatible with ream codebase
    
    * feat: move scripts to host and make the scripts risc0 compatible
    
    * feat: make all runs all operations
    
    * refactor: cargo fmt
    
    * refactor: small Makefile styling changes
    
    * fix: exclude unimplemented run-execution_payload
    
    * feat: add merkleize and commit

---
+++ host/subscripts/parse_log_to_table.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+OPERATION=$1
+
+LOG_FILE="logs/execution_$OPERATION.log"
+OUTPUT_FILE="summaries/summary_$OPERATION.md"
+
+# Table Header
+echo '| Operation | Test Case | Read Pre-State | Read Operation | Process | Merkleize | Commit | Execution Time |' > $OUTPUT_FILE
+echo '|-----------|-----------|----------------|----------------|---------|-----------|--------|----------------|' >> $OUTPUT_FILE
+
+awk '
+BEGIN {
+    op = "";
+    test_case = "";
+    read_pre_state_start = 0;
+    read_pre_state_end = 0;
+    read_operation_input_start = 0;
+    read_operation_input_end = 0;
+    process_operation_start = 0;
+    process_operation_end = 0;
+    merkleize_operation_start = 0;
+    merkleize_operation_end = 0;
+    commit_start = 0;
+    commit_end = 0;
+    execution_time = 0;
+}
+
+/\[.*\] Test case:/ {
+    op = $4;
+    gsub(/[\[\]]/, "", op)
+    test_case = $NF;
+}
+
+/read-pre-state:start:/ {
+    read_pre_state_start = $NF;
+}
+
+/read-pre-state:end:/ {
+    read_pre_state_end = $NF;
+}
+
+/read-operation-input:start:/ {
+    read_operation_input_start = $NF;
+}
+
+/read-operation-input:end:/ {
+    read_operation_input_end = $NF;
+}
+
+/process-operation:start:/ {
+    process_operation_start = $NF;
+}
+
+/process-operation:end:/ {
+    process_operation_end = $NF;
+}
+
+/merkleize-operation:start:/ {
+    merkleize_operation_start = $NF;
+}
+
+/merkleize-operation:end:/ {
+    merkleize_operation_end = $NF;
+}
+
+/commit:start:/ {
+    commit_start = $NF;
+}
+
+/commit:end:/ {
+    commit_end = $NF;
+}
+
+/execution time:/ {
+    execution_time = $NF;
+}
+
+/----- Cycle Tracker End -----/ {
+    printf "%s | %s | %d | %d | %d | %d | %d | %s |\n", op, test_case, read_pre_state_end-read_pre_state_start, read_operation_input_end-read_operation_input_start, process_operation_end-process_operation_start, merkleize_operation_end-merkleize_operation_start, commit_end-commit_start, execution_time >> "'$OUTPUT_FILE'"
+
+    # Re-initialize for next log
+    op = "";
+    test_case = "";
+    read_pre_state_start = 0;
+    read_pre_state_end = 0;
+    read_operation_input_start = 0;
+    read_operation_input_end = 0;
+    process_operation_start = 0;
+    process_operation_end = 0;
+    merkleize_operation_start = 0;
+    merkleize_operation_end = 0;
+    commit_start = 0;
+    commit_end = 0;
+    execution_time = 0;
+}
+' $LOG_FILE
